ebb040716bd6ff72db686441bd5e6ec45d56343c
Sherif Mohamed <shm23866@gmail.com>
2025-05-27 19:00:07 +0300

    chnage theme and create notes view

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+    );
+
+  }
+}
