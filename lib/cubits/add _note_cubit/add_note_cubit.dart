f264470050dbb8c2ade9f1f0b7cf4d1607a4f942
Sherif Mohamed <shm23866@gmail.com>
2025-05-30 02:50:40 +0300

    Provider add note cubit

--- lib/cubits/add _note_cubit/add_note_cubit.dart
+++ lib/cubits/add _note_cubit/add_note_cubit.dart
@@ -1,14 +1,27 @@
 
 
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:notes_app_project_tenth/cubits/add%20_note_cubit/add_note_state.dart';
 import 'package:notes_app_project_tenth/models/note_model.dart';
+import 'package:notes_app_project_tenth/widgets/constants.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState>{
 
   AddNoteCubit() : super(AddNoteInitial());
 
     addNote(NoteModel note){
+      emit(AddNoteLoading());
+      try{
+        var noteBox = Hive.box<NoteModel>(kNotesBox);
+
+        emit(AddNoteSuccess());
+        noteBox.add(note);
+
+      }catch(e){
+
+        AddNoteFailure(e.toString());
+      }
 
 
     }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive/hive.dart';
+
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app_project_tenth/models/note_model.dart';
 import 'package:notes_app_project_tenth/views/notes_view.dart';
 import 'package:notes_app_project_tenth/views/edit_%20note_view.dart';
 import 'package:notes_app_project_tenth/widgets/constants.dart';
 
+import 'cubits/add _note_cubit/add_note_cubit.dart';
+
 void main() async {
-  runApp(const NotesApp());
+  runApp( NotesApp());
 
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
@@ -17,21 +21,27 @@ void main() async {
 }
 
 class NotesApp extends StatelessWidget {
-  const NotesApp({super.key});
 
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        EditNoteView.id : (context) => EditNoteView(),
+    return MultiBlocProvider(
 
-      },
-      theme: ThemeData( brightness: Brightness.dark , fontFamily:"Poppins"),
-      debugShowCheckedModeBanner: false,
+      providers: [
 
-      home:NotesView() ,
+        BlocProvider(create: (context) => AddNoteCubit()),
+      ],
+      child: MaterialApp(
+        routes: {
+          EditNoteView.id: (context) => EditNoteView(),
+
+        },
+        theme: ThemeData(brightness: Brightness.dark, fontFamily: "Poppins"),
+        debugShowCheckedModeBanner: false,
+
+        home: NotesView(),
+      ),
     );
   }
 }
